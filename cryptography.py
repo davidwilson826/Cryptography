824b7d07f7e47b73b7e699873ce0f8c6de17d996
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-22 14:27:26 -0500

    Updated from Brython Server: 2/22/2016 2:27:26 PM

--- cryptography.py
+++ cryptography.py
@@ -1,12 +1,58 @@
 """
 cryptography.py
 Author: David Wilson
-Credit: http://stackoverflow.com/questions/1720421/how-to-append-list-to-second-list-concatenate-lists
+Credit: http://stackoverflow.com/questions/1720421/how-to-append-list-to-second-list-concatenate-lists,
+http://www.decalage.info/en/python/print_list
 
 Assignment:
 
 Write and submit a program that encrypts and decrypts user data.
 
+See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+"""
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+process = "e"#input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+def encrypt(message, key, process):
+    messnum = [associations.find(x) for x in message]
+    keynum = [associations.find(x) for x in key]
+    print(messnum)
+    print(keynum)
+    if len(messnum) > len(keynum):
+        keynumorig = keynum[:]
+        keynum *= len(messnum)//len(keynum)
+        keynum += keynumorig[:len(messnum)-len(keynum)]
+    elif len(keynum) > len(messnum):
+        keynum = keynum[:len(messnum)]
+    print(messnum)
+    print(keynum)
+    emessnum = [x+(process*keynum[messnum.index(x)]) for x in messnum]
+    print(emessnum)
+    emess = ''.join([associations[x%len(associations)] for x in emessnum])
+    print(emess)
+
+if process == "e":
+    message = "David Wilson"#input("Message: ")
+    key = "hello!3"#input("Key: ")
+    encrypt(message, key, 1)
+    
+elif process == "d":
+    message = input("Message: ")
+elif process == "q":
+    print("Goodbye!")
+else:
+    print("Did not understand command, try again.")
+    
+'''
+Back-up:
+
+"""
+cryptography.py
+Author: David Wilson
+Credit: http://stackoverflow.com/questions/1720421/how-to-append-list-to-second-list-concatenate-lists
+Assignment:
+Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
@@ -39,3 +85,4 @@ elif process == "q":
     print("Goodbye!")
 else:
     print("Did not understand command, try again.")
+'''
