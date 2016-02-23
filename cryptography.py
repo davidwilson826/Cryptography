a6469885584a803f963b98685daa16eae8b565dc
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-23 14:22:24 -0500

    Updated from Brython Server: February 23, 2016 2:22:23 PM EST

--- cryptography.py
+++ cryptography.py
@@ -18,6 +18,37 @@ def encrypt(message, key, process):
     messnum = [associations.find(x) for x in message]
     keynum = [associations.find(x) for x in key]
     if len(messnum) > len(keynum):
+        keynum = keynum*(len(messnum)//len(keynum))+keynum[:len(messnum)-len(keynum)]
+    elif len(keynum) > len(messnum):
+        keynum = keynum[:len(messnum)]
+    emessnum = [x+(process*keynum[messnum.index(x)]) for x in messnum]
+    emess = ''.join([associations[x%len(associations)] for x in emessnum])
+    print(emess)
+
+if process == "e":
+    message = input("Message: ")
+    key = input("Key: ")
+    encrypt(message, key, 1)
+elif process == "d":
+    message = input("Message: ")
+    key = input("Key: ")
+    encrypt(message, key, -1)
+elif process == "q":
+    print("Goodbye!")
+else:
+    print("Did not understand command, try again.")
+    
+'''
+Back-up:
+
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+process = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+def encrypt(message, key, process):
+    messnum = [associations.find(x) for x in message]
+    keynum = [associations.find(x) for x in key]
+    if len(messnum) > len(keynum):
         keynumorig = keynum[:]
         keynum *= len(messnum)//len(keynum)
         keynum += keynumorig[:len(messnum)-len(keynum)]
@@ -39,3 +70,4 @@ elif process == "q":
     print("Goodbye!")
 else:
     print("Did not understand command, try again.")
+'''
